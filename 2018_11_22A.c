8320a7efdb63aee66267504ac2870a2ab3e1bc7e
zhouchenhua <noatnu@163.com>
2018-11-22 22:30:23 +0800

    0000

--- 2018_11_22A.c
+++ 2018_11_22A.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 /*
 stdio.h 是一个头文件 (标准输入输出头文件) , #include 是一个预处理命令，用来引入头文件。 
 当编译器遇到 printf() 函数时，如果没有找到 stdio.h 头文件，会发生编译错误。
 */
 int main() {
 	printf("今天第一天! \n");
+
+	a1();
+
 	system("pause");
 	return 0;
+}
+int a1() {
+	char c1 = '1';
+	printf("int 存储大小 : %lu \n", sizeof(int));
+	return 2;
 }
